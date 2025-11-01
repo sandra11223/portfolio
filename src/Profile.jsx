6bd5a0eda2a2e6ea1201522169b50f16f4d85592
sandra11223 <sp151048@gmail.com>
2025-11-01 20:45:49 +0530

    transition

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -4,7 +4,6 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { IoLogoYoutube } from "react-icons/io5";
 import { FaGithub } from "react-icons/fa";
-import { FaBookOpen } from "react-icons/fa";
 
 const Profile = () => {
   return (
@@ -12,7 +11,7 @@ const Profile = () => {
     <div className='fixed top-0 left-0 w-full h-20 bg-white shadow-md z-50 flex p-7 font-bold text-lg gap-7 justify-between'>
   <div className='flex'>
     <img
-      className='w-10 h-10 mr-1 rounded-full'
+      className='w-10 h-7 mr-1 rounded-full'
       src="https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdpcmwlMjBwcm9maWxlfGVufDB8fDB8fHww&fm=jpg&q=60&w=3000"
       alt="profile"
     />
@@ -55,10 +54,7 @@ const Profile = () => {
         const c = document.getElementById('contact');
         if (c) c.scrollIntoView({ behavior: 'smooth' });
       }}
-      className="hover:text-purple-600 cursor-pointer transition duration-300"
-    >
-      CONTACT
-    </li>
+      className="hover:text-purple-600 cursor-pointer transition duration-300"> CONTACT</li>
   </ul>
 </div>
 
@@ -68,7 +64,7 @@ const Profile = () => {
         <h1 className='text-5xl font-bold text-black text-center pt-48 font-serif'> Hey I'm Sandra Surendran</h1><br></br>
         <h6 className='text-2xl text-black text-center mt-2 font-serif'>A Result-Oriented Web Developer building and managing Websites and Web <br></br> Applications that leads to the success of the overall product</h6><br></br>
         <div className='flex justify-center mt-8'>
-          <button className='w-40 h-12 font-bold text-white bg-purple-500 rounded font-bold'> PROFILES </button>
+          <button className='w-40 h-12 font-serif text-white bg-purple-500 rounded'> PROFILES </button>
         </div>
       </div>
         <div className='absolute left-5 top-1/2 transform -translate-y-1/2 bg-white p-4 rounded-lg shadow-lg flex flex-col gap-6 text-3xl text-black'>
@@ -76,7 +72,7 @@ const Profile = () => {
         <FaTwitter className='hover:text-sky-400 hover:scale-125 transition-all duration-300 cursor-pointer' />
         <IoLogoYoutube className='hover:text-red-600 hover:scale-125 transition-all duration-300 cursor-pointer' />
         <FaGithub className='hover:text-gray-800 hover:scale-125 transition-all duration-300 cursor-pointer' />
-        <FaBookOpen className='hover:text-purple-600 hover:scale-125 transition-all duration-300 cursor-pointer' />
+
       </div>
     </div>
   )
