ddcbbb7c6ae22219abb771a84400b71f185eb1e5
sandra11223 <sp151048@gmail.com>
2025-12-14 12:26:38 +0530

    transition

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { FaLinkedin, FaTwitter, FaGithub } from "react-icons/fa";
 import { IoLogoYoutube } from "react-icons/io5";
+import img from './sandra2.jpeg';
+
 
 const Profile = () => {
   return (
@@ -8,9 +10,11 @@ const Profile = () => {
       <div className="fixed top-0 left-0 w-full backdrop-blur-xl bg-white/10 border-b border-white/20 z-50 py-4 px-10 flex justify-between items-center">
         <div className="flex items-center gap-3">
           <img
-            src="https://images.unsplash.com/photo-1464863979621-258859e62245?auto=format&fit=crop&w=300&q=60"
-            alt="profile"
-            className="w-12 h-12 rounded-full border-2 border-white"/>
+  src={img}
+  alt="profile"
+  className="w-16 h-16 md:w-24 md:h-24 rounded-full border-2 border-white"
+/>
+
           <h1 className="text-2xl font-bold tracking-wider">Sandra Surendran</h1>
         </div>
 
@@ -22,8 +26,7 @@ const Profile = () => {
                 const section = document.getElementById(item.toLowerCase());
                 if (section) section.scrollIntoView({ behavior: "smooth" });
               }}
-              className="cursor-pointer hover:text-blue-400 transition"
-            >
+              className="cursor-pointer hover:text-blue-400 transition">
               {item}
             </li>
           ))}
@@ -34,20 +37,28 @@ const Profile = () => {
         <div className="absolute -top-20 -left-20 w-96 h-96 bg-blue-600/30 rounded-full blur-3xl animate-pulse"></div>
         <div className="absolute bottom-0 right-0 w-96 h-96 bg-purple-600/30 rounded-full blur-3xl animate-pulse delay-2000"></div>
 
-        <h1 className="text-6xl md:text-7xl font-extrabold text-center mb-6 relative z-10">
-          <span className="bg-gradient-to-r from-blue-300 to-purple-400 bg-clip-text text-transparent">
-            Hi, I'm Sandra
-          </span>
-        </h1>
 
-        <p className="text-lg md:text-2xl text-gray-300 max-w-3xl text-center leading-relaxed relative z-10 ">
-          A Passionate Web Developer focused on creating visually stunning,
-          responsive, and high-performance digital experiences.  
-          I build modern UI, optimize performance, and turn ideas into reality.
-        </p>
-        <button className="mt-10 px-10 py-3 bg-gradient-to-r from-blue-500 to-purple-500 rounded-xl text-lg font-semibold hover:scale-105 shadow-xl transition relative z-10">
-          View Profiles
-        </button>
+
+        <h1 className="text-6xl md:text-7xl font-extrabold text-center mb-6 relative z-10">
+  <span className="bg-gradient-to-r from-blue-300 to-purple-400 bg-clip-text text-transparent">
+    Hi, I'm Sandra 
+  </span>
+</h1>
+
+<p className="text-lg md:text-2xl text-gray-300 max-w-3xl text-center leading-relaxed relative z-10">
+  A passionate Web Developer from Kerala, India 🌍.  
+  I specialize in building modern, responsive UIs with React and TailwindCSS,  
+  and love turning creative ideas into functional digital experiences.  
+  When I’m not coding, I’m exploring design trends and learning new tech.
+</p>
+        <a 
+  href="https://vercel.com/s-projects-2829816e" 
+  target="_blank" 
+  rel="noreferrer">
+  <button className="mt-10 px-10 py-3 bg-gradient-to-r from-blue-500 to-purple-500 rounded-xl text-lg font-semibold hover:scale-105 shadow-xl transition relative z-10">
+    Explore My Works
+  </button>
+</a>
       </div>
       <div className="fixed left-6 top-1/2 -translate-y-1/2 flex flex-col gap-6 text-3xl z-50">
 
